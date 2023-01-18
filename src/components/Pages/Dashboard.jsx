1c137e3747be76a7afbaef857169a96410c3f246
gaston964 <gastongutierrez964@gmail.com>
2023-01-18 15:17:16 -0300

    Responsive 1

--- src/components/Pages/Dashboard.jsx
+++ src/components/Pages/Dashboard.jsx
@@ -5,13 +5,18 @@ import 'animate.css';
 const Dashboard = () => {
     return (
         <>
-        <div className="containerPadre">
-            <main className='dashboardMain'>
-                <section>
-                <div className="logo"><b>B<span>ie</span>nv<span>eni</span>dos</b></div>
-                </section>
-            </main>
-        </div>
+            <div className="containerPadre ">
+                <main className='dashboardMain'>
+                    <section className='hide'>
+                        <div className="logo"><b>B<span>ie</span>nv<span>eni</span>dos</b></div>
+                    </section>
+                    <section className='hidemax'>
+                        <div className="text">
+                            <h1 className="glow">Bienvenidos a mi Portfolio! </h1>
+                        </div>
+                    </section>
+                </main>
+            </div>
         </>
     )
 }
